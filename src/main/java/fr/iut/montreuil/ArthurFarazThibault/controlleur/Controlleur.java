4bcfafdd9e5f9c50b1a63258609443f85ca2fed8
Faraz1306 <farazsiddiqui02@yahoo.com>
2023-10-18 15:23:35 +0200

    commit faraz

--- src/main/java/fr/iut/montreuil/ArthurFarazThibault/controlleur/Controlleur.java
+++ src/main/java/fr/iut/montreuil/ArthurFarazThibault/controlleur/Controlleur.java
@@ -2,11 +2,8 @@ package fr.iut.montreuil.ArthurFarazThibault.controlleur;
 
 import fr.iut.montreuil.ArthurFarazThibault.modele.Case;
 import fr.iut.montreuil.ArthurFarazThibault.modele.Environnement;
-import fr.iut.montreuil.ArthurFarazThibault.modele.Fabrique;
 import fr.iut.montreuil.ArthurFarazThibault.modele.ForgePecheur ;
-import fr.iut.montreuil.ArthurFarazThibault.modele.SimpleFabriquePecheur ;
 
-import fr.iut.montreuil.ArthurFarazThibault.modele.ForgePecheur;
 import fr.iut.montreuil.ArthurFarazThibault.vue.vueTerrain;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -166,19 +163,19 @@ public class Controlleur implements Initializable {
     @FXML
     public void placerPecheur(MouseEvent event) {
         if (groupeRadio.getSelectedToggle().equals(selectionnerHarponneur)) {
-            Fabrique.creerPecheur(event.getX(), event.getY(), 1, environnement);
+            ForgePecheur.creerPecheur(event.getX(), event.getY(), 1);
             //fP.creerPecheur(event.getX(), event.getY(), "harponneur") ;
         }
         else if (groupeRadio.getSelectedToggle().equals(selectionnerLanceur)) {
-            Fabrique.creerPecheur(event.getX(), event.getY(), 2, environnement);
+            ForgePecheur.creerPecheur(event.getX(), event.getY(), 2);
             //fP.creerPecheur(event.getX(), event.getY(), "lanceur") ;
         }
         else if (groupeRadio.getSelectedToggle().equals(selectionnerArcher)) {
-            Fabrique.creerPecheur(event.getX(), event.getY(), 3, environnement);
+            ForgePecheur.creerPecheur(event.getX(), event.getY(), 3);
             //fP.creerPecheur(event.getX(), event.getY(), "archer") ;
         }
         else {
-            Fabrique.creerPecheur(event.getX(), event.getY(), 4, environnement);
+            ForgePecheur.creerPecheur(event.getX(), event.getY(), 4);
             //fP.creerPecheur(event.getX(), event.getY(), "tremailleur") ;
         }
 
--- src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/ForgePecheur.java
+++ src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/ForgePecheur.java
@@ -1,47 +1,11 @@
 package fr.iut.montreuil.ArthurFarazThibault.modele;
 
-<<<<<<< HEAD
-import javafx.beans.property.SimpleListProperty;
-import fr.iut.montreuil.ArthurFarazThibault.modele.Environnement ;
-
-public class ForgePecheur {
-
-    private Environnement env ;
-    private SimpleFabriquePecheur fabriquePecheur ;
-
-    public ForgePecheur ( Environnement env, SimpleFabriquePecheur fabriquePecheur ) {
-        this.env = env ;
-        this.fabriquePecheur = fabriquePecheur ;
-    }
-
-    public Pecheur creerPecheur ( double x, double y, String nom  ) {
-        return fabriquePecheur.creerPecheur(x,y,env,nom);
-    }
-
-    /*
-    public Pecheur creerHarponneur ( double x, double y ) {
-        return fabriquePecheur.creerPecheur(x,y,env,"harponneur");
-    }
-
-    public Pecheur creerArcher ( double x, double y ) {
-        return fabriquePecheur.creerPecheur(x,y,env,"archer");
-    }
-
-    public Pecheur creerLanceur ( double x, double y ) {
-        return fabriquePecheur.creerPecheur(x,y,env,"lanceur");
-    }
-
-    public Pecheur creerTremailleur ( double x, double y ) {
-        return fabriquePecheur.creerPecheur(x,y,env,"tremailleur");
-    }
-
-*/
-
-=======
 import fr.iut.montreuil.ArthurFarazThibault.modele.pecheurs.Archer;
 import fr.iut.montreuil.ArthurFarazThibault.modele.pecheurs.Harponneur;
 import fr.iut.montreuil.ArthurFarazThibault.modele.pecheurs.Lanceur;
 import fr.iut.montreuil.ArthurFarazThibault.modele.pecheurs.Tremailleur;
+import javafx.beans.property.SimpleListProperty;
+import fr.iut.montreuil.ArthurFarazThibault.modele.Environnement ;
 
 public class ForgePecheur {
 
@@ -75,6 +39,5 @@ public class ForgePecheur {
         }
 
     }
->>>>>>> f7b501e40788cfeb904626a43bcf5da56c303996
 
 }
