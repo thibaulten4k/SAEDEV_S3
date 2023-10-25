f1e82d2978eeeb9b3eba642615e13291d51fefe1
Robin Castel <robincastel2003@gmail.com>
2023-10-25 16:28:39 +0200

    remplacement effets par null dans constructeurs projectiles

--- src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/projectiles/Chien.java
+++ src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/projectiles/Chien.java
@@ -7,7 +7,7 @@ import fr.iut.montreuil.ArthurFarazThibault.modele.deplacements.TeteChercheuse;
 public class Chien extends Projectile {
 
     public Chien(int x, int y, Poisson poisson) {
-        super(x, y, 10, 7, 14, 60, 60, poisson, new AucunEffet());
+        super(x, y, 10, 7, 14, 60, 60, poisson, null);
         this.setComportement(new TeteChercheuse(this, poisson));
     }
 
