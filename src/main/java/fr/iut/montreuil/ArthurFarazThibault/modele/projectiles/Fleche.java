cf6af0a743e82679b23b12fe74e14046686548ce
Robin Castel <robincastel2003@gmail.com>
2023-10-25 16:25:50 +0200

    remplacement effets par null dans constructeurs projectiles

--- src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/projectiles/Fleche.java
+++ src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/projectiles/Fleche.java
@@ -7,7 +7,7 @@ import fr.iut.montreuil.ArthurFarazThibault.modele.deplacements.LigneDroite;
 public class Fleche extends Projectile {
 
     public Fleche (int x, int y, Poisson poisson) {
-        super(x, y, 16, 7, 12, 60, 60, poisson, new AucunEffet());
+        super(x, y, 16, 7, 12, 60, 60, poisson, null);
         this.setComportement(new LigneDroite(this, poisson));
     }
 
--- src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/projectiles/Harpon.java
+++ src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/projectiles/Harpon.java
@@ -7,7 +7,7 @@ import fr.iut.montreuil.ArthurFarazThibault.modele.deplacements.LigneDroite;
 public class Harpon extends Projectile {
 
     public Harpon (int x, int y, Poisson poisson) {
-        super(x, y, 12, 13, 8, 75, 15, poisson, new AucunEffet());
+        super(x, y, 12, 13, 8, 75, 15, poisson, null);
         this.setComportement(new LigneDroite(this, poisson));
     }
 
