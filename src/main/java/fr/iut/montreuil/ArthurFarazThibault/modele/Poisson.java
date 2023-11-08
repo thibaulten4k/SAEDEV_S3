02a1f44c1ea4a0067c141e281f65812d72791b00
Arthur <arepetti@condorcet93.fr>
2023-11-08 15:55:39 +0100

    commit correction de bug RObain des bois, Robain est notre messi, Robain est l'amour, Robain djiojd

--- src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/Poisson.java
+++ src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/Poisson.java
@@ -31,11 +31,10 @@ public class Poisson extends ActeurMobile {
             this.meurt();
     }
 
-    public void meurt(){
-        Environnement.getInstance().setPvPropertyValue(Environnement.getInstance().getPvPropertyValue() - 5);
+    public void meurt() {
         this.setPv(0);
         this.setRecompense(recompense/2);
-        //Environnement.getInstance().subirDegat(degat);
+        Environnement.getInstance().subirDegat(degat);
     }
 
     public String toString() { return "Poisson : " + this.id; }
