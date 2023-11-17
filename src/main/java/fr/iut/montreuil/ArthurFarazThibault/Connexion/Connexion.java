7f3459d6370e94288a82c7dd63760f421c1c20de
Robin Castel <robincastel2003@gmail.com>
2023-11-17 09:32:51 +0100

    commit

--- src/main/java/fr/iut/montreuil/ArthurFarazThibault/Connexion/Connexion.java
+++ src/main/java/fr/iut/montreuil/ArthurFarazThibault/Connexion/Connexion.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Connexion {
     public static void main(String[] args) {
         try {
-        Connection con = DriverManager.getConnection("jdbc:postgresql://database-etudiants.iut.univ-paris8.fr/localhost/dutinfopw201632", "dutinfopw201632", "jynuhuby");
+        Connection con = DriverManager.getConnection("jdbc:postgresql://database-etudiants.iut.univ-paris8.fr/thouemabe", "thouemabe", "Ttt458800");
         System.out.println("Connexion établie");
         }
         catch(SQLException e) {
