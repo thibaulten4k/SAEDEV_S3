7494f845e9103f2f27f37673e2d8f67fe05a79e2
Faraz1306 <farazsiddiqui02@yahoo.com>
2023-11-17 09:38:44 +0100

    correction du jeu Incroyable !

--- src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/VagueAvecPause.java
+++ src/main/java/fr/iut/montreuil/ArthurFarazThibault/modele/VagueAvecPause.java
@@ -1,5 +1,5 @@
 package fr.iut.montreuil.ArthurFarazThibault.modele;
-
+/*
 public class VagueAvecPause extends StrategieVague {
 
     public VagueAvecPause ( int tauxSpawn, int tauxSaumon, int tauxAlose, int tauxLamproie, int tauxEsturgeon, int objectif, int delai ) {
@@ -9,3 +9,4 @@ public class VagueAvecPause extends StrategieVague {
     }
 
 }
+*/
